3546259f5cbf4e99117e6cbc7bf65acb2425ab18
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-23 16:31:01 -0300

    Adding the about view

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello World from rango app.')
+
+def about(request):
+    return HttpResponse('Rango says: Here is the about page.')
