12d523924ee4c1fff7fd783ac2e10141ea1da859
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-23 23:32:18 -0300

    Adding links from index page to about page and vice-versa

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World from rango app.')
+    return HttpResponse('Hello World from rango app. Go to <a href="/rango/about">about page</a>')
 
 def about(request):
-    return HttpResponse('Rango says: Here is the about page.')
+    return HttpResponse('Rango says: Here is the about page. Go to <a href="/rango/">main page</a>')
