53cc05f7f91df792b9f21c682734d5590a840be9
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-24 15:57:01 -0300

    Configuring django admin

---
+++ rango/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from rango import models
+
+admin.site.register(models.Category)
+admin.site.register(models.Page)
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,7 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls'))
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
