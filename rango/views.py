b85948f4b9d4835dc9dfa15c5ec0e2ab7e64b264
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-24 20:06:32 -0300

    Alternative method of showing a human readable category in URL, but more reliable, using id to get category object

--- rango/views.py
+++ rango/views.py
@@ -3,11 +3,25 @@ from django.shortcuts import render_to_response
 from rango import models
 
 
+def generate_url(id_, value):
+    """Generates an url by combining id of object and some string value."""
+    return "%s_%s" % (id_, value.replace(' ', '_'))
+
+
+def get_id_from_url(url):
+    """Get id from url in the format 42_Some_text."""
+    try:
+        id_ = int(url.split('_')[0])
+    except ValueError:
+        id_ = None
+    return id_
+
+
 def index(request):
     context = RequestContext(request)
     categories = models.Category.objects.order_by('-likes')[:5]
     for category in categories:
-        category.url = category.name.replace(' ', '_')
+        category.url = generate_url(category.id, category.name)
     pages = models.Page.objects.order_by('-views')[:5]
     context_dict = {'categories': categories, 'pages': pages}
     return render_to_response('rango/index.html', context_dict, context)
@@ -19,15 +33,12 @@ def about(request):
 
 def category(request, category_name_url):
     context = RequestContext(request)
-    # category = models.Category.
-    category_name = category_name_url.replace('_', ' ')
-    context_dict = {'category_name': category_name}
+    category_id = get_id_from_url(category_name_url)
     try:
-        category = models.Category.objects.get(name=category_name)
+        category = models.Category.objects.get(id=category_id)
         pages = models.Page.objects.filter(category=category)
-        context_dict['category'] = category
-        context_dict['pages'] = pages
+        context_dict = {'category': category, 'pages': pages}
     except models.Category.DoesNotExist:
-        pass
+        context_dict = {'category_name_url': category_name_url}
 
     return render_to_response('rango/category.html', context_dict, context)
