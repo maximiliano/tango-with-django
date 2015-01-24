e4bde9142fe9a9f4282574d6514ba53b0146e05a
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-24 18:40:52 -0300

    Showing all categories in index page

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango import models
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'bold_message': 'I am bold font from the context.'}
+    categories = models.Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': categories}
     return render_to_response('rango/index.html', context_dict, context)
 
 
