06eec58304a5280324362edd48fb5f903e2e9d8e
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-24 17:02:18 -0300

    Applying pep8 on some files

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -86,7 +86,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -96,7 +96,7 @@ SECRET_KEY = 'ghqqk!g*bx!ze+pomr@syujhs%j3&!%@9=g0(u8fs31uig+58h'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'tango_with_django.views.home', name='home'),
     # url(r'^tango_with_django/', include('tango_with_django.foo.urls')),
