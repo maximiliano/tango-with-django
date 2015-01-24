ae52f9c551da9035255c4957b0bb6594bbc31317
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2015-01-24 17:10:50 -0300

    Customizing admin Page list

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from rango import models
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 admin.site.register(models.Category)
-admin.site.register(models.Page)
+admin.site.register(models.Page, PageAdmin)
